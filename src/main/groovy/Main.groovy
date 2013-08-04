baca2f1a68ed28cc7911b46069d92a248b734044
ryotakato <ryotakato4@gmail.com>
2013-08-04 18:19:07 +0900

    make moving lifes

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -4,30 +4,58 @@ import processing.core.*;
 
 
 class Main extends PApplet {
-  
-  def balls = []
-
-  void setup(){
-    // create dispaly
-    size 500,500
-    
-    frameRate 60
-  }
-
-  void draw() {
-    background 150
-
-    if (random(1) < 0.01) {
-      balls.add([x: random(200), y: 0])
-    }
-    balls.each {
-      it.y++
-      ellipse(it.x, it.y, 10, 10)
-    }
-  }
 
   static main(args) {
     PApplet.main("Main")
   }
+
+
+  def plants = []
+  def herbivores = []
+  def carnivores = []
+
+  void setup(){
+    // create dispaly
+    size 500,500
+    frameRate 60
+    
+    // create life
+    10.times {
+      plants.add([x: random(500), y: random(500), c:Color.GREEN])
+    }
+    10.times {
+      herbivores.add([x: random(500), y: random(500), c:Color.YELLOW])
+    }
+    10.times {
+      carnivores.add([x: random(500), y: random(500), c:Color.RED])
+    }
+  }
+
+  void draw() {
+    background(Color.BLACK.getRGB())
+
+    plants.each {
+      it.x++;
+      it.y++;
+      stroke(it.c.getRGB());
+      fill(it.c.getRGB());
+      ellipse(it.x as float, it.y as float, 10, 10);
+    }
+    herbivores.each {
+      it.x++;
+      it.y++;
+      stroke(it.c.getRGB());
+      fill(it.c.getRGB());
+      ellipse(it.x as float, it.y as float, 10, 10);
+    }
+    carnivores.each {
+      it.x++;
+      it.y++;
+      stroke(it.c.getRGB());
+      fill(it.c.getRGB());
+      ellipse(it.x as float, it.y as float, 10, 10);
+    }
+  }
+
 }
 
